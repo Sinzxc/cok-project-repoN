dfb6a1a2e7acd330e63a7769f092f1f340ef9e1e
Sinzxc <nik.orig@mail.ru>
2023-10-07 18:28:24 +0300

    add main menu and 2 COK

---
+++ script/routes.js
@@ -0,0 +1,146 @@
+const routes = [
+  {
+    path: '/',
+    getTemplate: (params) => 'modules/main-menu/index.html'
+  },
+  {
+    path: '/tourism',
+    getTemplate: (params) => 'modules/tourism/tourism.html'
+  },
+  {
+    path: '/clothes-belts',
+    getTemplate: (params) => 'modules/belts/belts.html'
+  },
+  {
+    path: '/clothes-shoulders',
+    getTemplate: (params) => 'modules/shoulders/shoulders.html'
+  },
+  {
+    path: '/phisculture',
+    getTemplate: (params) => 'modules/phisculture/phisculture.html'
+  },
+  {
+    path: '/hotel',
+    getTemplate: (params) => 'modules/hotel/hotel.html'
+  },
+  {
+    path: '/phisculture_1',
+    getTemplate: (params) => 'modules/phisculture_1/phisculture_1.html'
+  },
+  {
+    path: '/cooking',
+    getTemplate: (params) => 'modules/cooking/cooking.html'
+  },
+  {
+    path: '/cocktails',
+    getTemplate: (params) => 'modules/cocktails/cocktails.html'
+  },
+  
+  {
+    path: '/clothes-belts-dynamic-infographic',
+    getTemplate: (params) => 'modules/belts/dynamic-infographic/dynamic-infographic.html'
+  },
+  {
+    path: '/clothes-belts-interactive-simulator',
+    getTemplate: (params) => 'modules/belts/interactive-simulator/interactive-simulator.html'
+  },
+  {
+    path: '/clothes-belts-virtual-simulator',
+    getTemplate: (params) => 'modules/belts/virtual-simulator/virtual-simulator.html'
+  },
+  {
+    path: '/clothes-belts-video-lesson',
+    getTemplate: (params) => 'modules/belts/video-lesson/video.html'
+  },
+
+  {
+    path: '/clothes-shoulders-dynamic-infographic',
+    getTemplate: (params) => 'modules/shoulders/dynamic-infographic/dynamic-infographic.html'
+  },
+  {
+    path: '/clothes-shoulders-interactive-simulator',
+    getTemplate: (params) => 'modules/shoulders/interactive-simulator/interactive-simulator.html'
+  },
+  {
+    path: '/clothes-shoulders-virtual-simulator',
+    getTemplate: (params) => 'modules/shoulders/virtual-simulator/virtual-simulator.html'
+  },
+
+  {
+    path: '/tourism-interactive-simulator',
+    getTemplate: (params) => 'modules/tourism/interactive-simulator/interactive-simulator.html'
+  },
+  {
+    path: '/tourism-virtual-simulator',
+    getTemplate: (params) => 'modules/tourism/virtual-simulator/virtual-simulator.html'
+  },
+  {
+    path: '/tourism-video-lesson',
+    getTemplate: (params) => 'modules/tourism/video-lesson/video.html'
+  },
+
+  {
+    path: '/phisculture-video-lesson',
+    getTemplate: (params) => 'modules/phisculture/video-lesson/video.html'
+  },
+  {
+    path: '/phisculture-virtual-simulator',
+    getTemplate: (params) => 'modules/phisculture/virtual-simulator/virtual-simulator.html'
+  },
+  {
+    path: '/phisculture-interactive-simulator',
+    getTemplate: (params) => 'modules/phisculture/interactive-simulator/interactive-simulator.html'
+  },
+
+  {
+    path: '/hotel-dynamic-infographic',
+    getTemplate: (params) => 'modules/hotel/dynamic-infographic/dynamic-infographic.html'
+  },
+  {
+    path: '/hotel-virtual-simulator',
+    getTemplate: (params) => 'modules/phisculture/virtual-simulator/virtual-simulator.html'
+  },
+  {
+    path: '/hotel-interactive-simulator',
+    getTemplate: (params) => 'modules/phisculture/interactive-simulator/interactive-simulator.html'
+  },
+
+  {
+    path: '/phisculture_1-video-lesson',
+    getTemplate: (params) => 'modules/phisculture_1/video-lesson/video.html'
+  },
+  {
+    path: '/phisculture_1-virtual-simulator',
+    getTemplate: (params) => 'modules/phisculture_1/virtual-simulator/virtual-simulator.html'
+  },
+  {
+    path: '/phisculture_1-interactive-simulator',
+    getTemplate: (params) => 'modules/phisculture_1/interactive-simulator/interactive-simulator.html'
+  },
+
+  {
+    path: '/cooking-video-lesson',
+    getTemplate: (params) => 'modules/cooking/video-lesson/video.html'
+  },
+  {
+    path: '/cooking-virtual-simulator',
+    getTemplate: (params) => 'modules/cooking/virtual-simulator/virtual-simulator.html'
+  },
+  {
+    path: '/cooking-interactive-simulator',
+    getTemplate: (params) => 'modules/cooking/interactive-simulator/interactive-simulator.html'
+  },
+
+  {
+    path: '/cocktails-video-lesson',
+    getTemplate: (params) => 'modules/cocktails/video-lesson/video.html'
+  },
+  {
+    path: '/cocktails-virtual-simulator',
+    getTemplate: (params) => 'modules/cocktails/virtual-simulator/virtual-simulator.html'
+  },
+  {
+    path: '/cocktails-interactive-simulator',
+    getTemplate: (params) => 'modules/cocktails/interactive-simulator/interactive-simulator.html'
+  },
+];
