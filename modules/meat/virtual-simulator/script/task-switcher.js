6302f47d53b9da44aa5931d1f23b6e5fa06743bf
timur <abdurakhmanov.timur.01@gmail.com>
2023-11-25 16:43:23 +0300

    Fix meat

--- modules/meat/virtual-simulator/script/task-switcher.js
+++ modules/meat/virtual-simulator/script/task-switcher.js
@@ -1,7 +1,7 @@
 const circles = document.querySelectorAll('.circle');
 const taskWraps = document.querySelectorAll('.practice-part-wrap')
 const numNum = document.querySelector('.num-now')
-console.log('dsfsdkjf')
+
 let i;
 let numtaskNow = 1
 for(i = 0; i < circles.length; i++) {
@@ -14,10 +14,7 @@ for(i = 0; i < circles.length; i++) {
             circles[i].classList.remove('circle_active');
             taskWraps[i].classList.remove('practice-part-wrap--active')
         }
-        document.querySelector('.clear-btn').style.cssText = "display: none"
-        document.querySelector('.clear-btn-2').style.cssText = "display: none"
-        document.querySelector('.clear-btn-3').style.cssText = "display: none"
-        
+
         numNum.innerHTML = button[5]
         numtaskNow = button[5] //номер задания
         quationSwitcher() 
@@ -45,9 +42,7 @@ backBtn.addEventListener('click',()=> {
         document.querySelector(`#task-${numtaskNow}`).classList.add('practice-part-wrap--active');
         numNum.innerHTML = numtaskNow
         circles[numtaskNow - 1].classList.add('circle_active');
-        document.querySelector('.clear-btn').style.cssText = "display: none"
-        document.querySelector('.clear-btn-2').style.cssText = "display: none"
-        document.querySelector('.clear-btn-3').style.cssText = "display: none"
+
         quationSwitcher() 
         if(numtaskNow == circles.length) {
             nextBtn.innerHTML = "Завершить"
@@ -80,9 +75,7 @@ nextBtn.addEventListener('click',()=> {
         document.querySelector(`#task-${numtaskNow}`).classList.add('practice-part-wrap--active');
         numNum.innerHTML = numtaskNow
         circles[numtaskNow - 1].classList.add('circle_active');
-        document.querySelector('.clear-btn').style.cssText = "display: none"
-        document.querySelector('.clear-btn-2').style.cssText = "display: none"
-        document.querySelector('.clear-btn-3').style.cssText = "display: none"
+
         quationSwitcher() 
         if(numtaskNow == circles.length) {
             nextBtn.innerHTML = "Завершить"
@@ -95,52 +88,30 @@ nextBtn.addEventListener('click',()=> {
 
 function RaschitatiBalli() {
     let ball = 0;
-    if(isCor1 && isCor2 && isCor3) ball++
-    if(document.querySelector('.true-answer-2-1').checked && 
-        !(document.querySelector('.true-answer-2-2').checked) &&
-        document.querySelector('.true-answer-2-3').checked && 
-        document.querySelector('.true-answer-2-4').checked) ball++
-    if(document.querySelector('.true-answer-3-1').checked && 
-        !(document.querySelector('.true-answer-3-2').checked) &&
-        document.querySelector('.true-answer-3-3').checked) ball++
-    if(isCor12 && isCor22 && isCor32) ball++
-    if( document.querySelector('.true-answer-5-1').checked && 
-    document.querySelector('.true-answer-5-2').checked) ball++
-    if(isCor13 && isCor23 && isCor33) ball++
-    if(document.querySelector('.answer1-hostel').value == 147810 && document.querySelector('.answer2-rest').value == 25 &&
-    document.querySelector('.answer3-tour-agent').value == 369) ball++
-    if(document.querySelector('.answer-8-1').value == 2 && document.querySelector('.answer-8-2').value == 3 &&
-    document.querySelector('.answer-8-3').value == 1) ball++
+
+    if(document.querySelector('.answer-1-1').value == 1 && document.querySelector('.answer-1-2').value == 5 &&
+    document.querySelector('.answer-1-3').value == 4 && document.querySelector('.answer-1-4').value == 6 && document.querySelector('.answer-1-5').value == 2 && document.querySelector('.answer-1-6').value == 3) ball++
+    if(document.querySelector('.answer-2-1').value == 4 && document.querySelector('.answer-2-2').value == 3 &&
+    document.querySelector('.answer-2-3').value == 2 && document.querySelector('.answer-2-4').value == 1) ball++
+    if(document.querySelector('.answer-3').value == "майонез" || document.querySelector('.answer-3').value == "Майонез") ball++
+    if(document.querySelector('.answer-4').value == "волованы с курицей" || document.querySelector('.answer-4').value == "Волованы с курицей") ball++
+    if(document.querySelector('.answer-5').value == "150" || document.querySelector('.answer-5').value == "150") ball++
+    if(document.querySelector('.answer-6').value == "30" || document.querySelector('.answer-6').value == "30") ball++
+    if(document.querySelector('.answer-7').value == "Галантин" || document.querySelector('.answer-7').value == "галантин") ball++
+    if(document.querySelector('.answer-8').value == "Заливное из курицы" || document.querySelector('.answer-8').value == "заливное из курицы") ball++
 
     document.querySelector('.result-text').innerHTML = "Количество баллов: "+ball
 }
 
 function quationSwitcher() {
     if(numtaskNow == 1) {
-        document.querySelector('.task-text').innerHTML = "Установите соответствия"
-        document.querySelector('.clear-btn').style.cssText = "display: block"
+        document.querySelector('.task-text').innerHTML = "Установите соответствие между правильностью использования разделочных досок при приготовлении кулинарной продукции:"
     }
     else if(numtaskNow == 2) {
-        document.querySelector('.task-text').innerHTML = "Какие качества должны быть у хорошего хозяина / хозяйки?"
+        document.querySelector('.task-text').innerHTML = "Установите соответствие между нормами расхода соли и специй для приготовления холодных блюд и закусок:"
     }
-    else if(numtaskNow == 3) {
-        document.querySelector('.task-text').innerHTML = "Задание «верно-неверно»"
+    else if(numtaskNow == 3 || numtaskNow == 4 || numtaskNow == 5 || numtaskNow == 7 || numtaskNow == 8 || numtaskNow == 6) {
+        document.querySelector('.task-text').innerHTML = "Ответьте на вопрос:"
     }
-    else if(numtaskNow == 4) {
-        document.querySelector('.task-text').innerHTML = "Установите соответствия"
-        document.querySelector('.clear-btn-2').style.cssText = "display: block"
-    }
-    else if(numtaskNow == 5) {
-        document.querySelector('.task-text').innerHTML = "Найди правильные ответы"
-    }
-    else if(numtaskNow == 6) {
-        document.querySelector('.task-text').innerHTML = "Обучающимся предложен список предложений различных гостиниц, ресторанов и туристических агентств. Задание сопоставить каждое предложение с соответствующей организацией."
-    }
-    else if(numtaskNow == 7) {
-        document.querySelector('.task-text').innerHTML = "Сопоставьте виды номеров гостиницы с их описанием"
-        document.querySelector('.clear-btn-3').style.cssText = "display: block"
-    }
-    else if(numtaskNow == 8) {
-        document.querySelector('.task-text').innerHTML = "Необходимо сопоставить разработанное меню для гостей из Китая, Японии и Кавказа с их с предпочтениями и потребностями"
-    }
+
 }
